ad1c9ab3876eb01bd0d082721fa30308abb6b7b2
Stacy Herold <telugumallastacy@gmail.com>
2022-12-17 22:16:20 +0530

    Done Tile class

--- src/com/chess/engine/board/chessTile.java
+++ src/com/chess/engine/board/chessTile.java
@@ -1,13 +1,33 @@
+package com.chess.engine.board;
+
+import com.chess.engine.pieces.chessPiece;
+import com.sun.org.apache.xml.internal.security.utils.resolver.ResourceResolverException;
+import jdk.nashorn.internal.ir.annotations.Immutable;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class chessTile {
 
-    int tileCoordinate;
+    protected final int tileCoordinate;
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+        for(int i=0; i<64; i++) {
+            emptyTileMap.put(i,new EmptyTile(i));
+        }
+
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
     chessTile(int tileCoordinate){
         this.tileCoordinate=tileCoordinate;
     }
     public abstract boolean isTileOccupied();
     public abstract chessPiece getPiece();
     public static final class EmptyTile extends chessTile{
-        EmptyTile(int coordinate){
+        EmptyTile(final int coordinate){
             super(coordinate);
         }
         @Override
@@ -22,7 +42,7 @@ public abstract class chessTile {
     }
 
     public static final class OccupiedTile extends chessTile{
-        chessPiece pieceOnTile;
+        private final chessPiece pieceOnTile;
         OccupiedTile(int tileCoordinate,chessPiece pieceOnTile) {
             super(tileCoordinate);
             this.pieceOnTile = pieceOnTile;
