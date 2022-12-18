244baece324a11490580085d024fe37838b4b5c8
Stacy Herold <80808708+gamysterio137@users.noreply.github.com>
2022-12-18 11:15:20 +0530

    Add files via upload

---
+++ chessTile.java
@@ -0,0 +1,65 @@
+package com.chess.engine.board;
+
+import com.chess.engine.pieces.chessPiece;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class chessTile {
+    protected final int tileCoordinate;
+    private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+        for(int i=0; i<64; i++) {
+            emptyTileMap.put(i,new EmptyTile(i));
+        }
+        Collections.unmodifiableMap(emptyTileMap);
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    public static chessTile createTile(final int tileCoordinate, final chessPiece piece){
+    return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
+    }
+
+    private chessTile(int tileCoordinate){
+        this.tileCoordinate=tileCoordinate;
+    }
+    public abstract boolean isTileOccupied();
+    public abstract chessPiece getPiece();
+    public static final class EmptyTile extends chessTile{
+        private EmptyTile(final int coordinate){
+            super(coordinate);
+        }
+        @Override
+        public boolean isTileOccupied(){
+            return false;
+
+        }
+        @Override
+        public chessPiece getPiece(){
+            return null;
+        }
+    }
+
+    public static final class OccupiedTile extends chessTile{
+        private final chessPiece pieceOnTile;
+        private OccupiedTile(int tileCoordinate,chessPiece pieceOnTile) {
+            super(tileCoordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied(){
+            return true;
+        }
+
+        @Override
+        public chessPiece getPiece(){
+            return this.pieceOnTile;
+
+        }
+
+    }
+}
